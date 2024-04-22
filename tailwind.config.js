9aeb0742f3163b5050681dedd75c9421c840430b
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-04-22 21:45:07 +0530

    style: init custom google fonts on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Rubik", "sans-serif"],
+      kalnia: ["Kalnia", "serif"],
+    },
   },
   plugins: [],
 };
