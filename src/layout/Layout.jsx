98f52e632c2f21fbf0bec8ef4178c3750b408b27
Abdul Khalid <mezmerizingbeats2004@gmail.com>
2024-04-28 22:35:00 +0530

    style: add flex for layout base

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div className="bg-gray-50 text-gray-800 text-sm">
+    <div className="bg-gray-50 flex flex-col justify-between min-h-screen text-gray-800 text-sm">
       <Header />
       <Outlet />
       <Footer />
